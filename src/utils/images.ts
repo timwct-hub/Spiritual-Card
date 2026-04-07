d021347c73cfd93c5550794a84b0b8fb9ffd74ec
timwct-hub <tim.yt202388@gmail.com>
2026-04-07 21:53:49 +0800

    Update images.ts

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,7 +1,3 @@
-export const cardImages = import.meta.glob('/public/cards/*.png', { eager: true, query: '?url', import: 'default' }) as Record<string, string>;
-
-export const getImageUrl = (imagePath: string) => {
-  // imagePath is like "/cards/1.png"
-  const fullPath = `/public${imagePath}`;
-  return cardImages[fullPath] || imagePath;
+export const getImageUrl = (imagePath: string): string => {
+  return imagePath;
 };
